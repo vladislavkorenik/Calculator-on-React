6919bc1d3941258d2b1a67bfd86a9f4b7f01a58d
Vladislav Korenik <vladyha66551@gmail.com>
2019-08-15 15:31:20 +0300

    added buttons: . % ce c

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -5,22 +5,26 @@ import './buttons.css'
 const Buttons = () => {
     return (
         <div className = 'buttons'>
+            <button>С</button>
+            <button>СE</button>
+            <button>%</button>
+            <button>*</button>
             <button>1</button>
             <button>2</button>
             <button>3</button>
-            <button>*</button>
+            <button>/</button>
             <button>4</button>
             <button>5</button>
             <button>6</button>
-            <button>/</button>
+            <button>-</button>
             <button>7</button>
             <button>8</button>     
             <button>9</button>
             <button>+</button>
             <button>&radic;</button>
             <button>0</button>
+            <button>.</button>
             <button>=</button>
-            <button>-</button>
         </div>
     );
 };
