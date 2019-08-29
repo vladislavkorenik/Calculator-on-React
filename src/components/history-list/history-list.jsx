cfdb61edce5df55822a8de254123770e01b63956
Vladislav Korenik <vladyha66551@gmail.com>
2019-08-29 23:04:51 +0300

    now history work correctly

--- src/components/history-list/history-list.jsx
+++ src/components/history-list/history-list.jsx
@@ -1,39 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './history-list.css'
 
-import HistoryListItem from '../history-list-item'
 import CommonButton from '../common-button'
 
 
-export default class HistoryList extends Component { 
+const HistoryList = ({ visibility, clearAll, list }) => {
+    let className = 'history-list invisibility';
 
-    state = {
-        className: 'history-list invisibility',
-        list: [] 
-    }
-    
-    clearAll = () => {
-        localStorage.removeItem('arrList');
-        this.setState({
-           list: [] 
-        });
+    if(visibility) {
+        className = 'history-list';
     }
 
-    componentDidMount  = () => {
-        let arrList = JSON.parse(localStorage.getItem('arrList')); 
-        this.setState({
-            list: arrList === null ? [] : arrList.map( item => <HistoryListItem key = { item.value } props = { item } />)
-         });
-    }
-    
-    render() {
-        return (
-            <div className = {  JSON.parse(localStorage.getItem('visibility')) ? 'history-list' : this.state.className }>
-                <ul>
-                    { this.state.list }
-                </ul>
-                <CommonButton func = { this.clearAll } value = { 'Clear' }/>
-            </div>   
-        );
-    }; 
-};
+    return (
+        <div className = {className}>
+            <ul>
+                {list}
+            </ul>
+            <CommonButton func = { clearAll } value = { 'Clear' }/>
+        </div>   
+    );
+}
+
+export default HistoryList
